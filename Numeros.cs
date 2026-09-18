58db838833120781b31db47cb72d5dbbf2e40aea
geinergarizabalo43-hub <geinergarizabalo43@gmail.com>
2026-09-18 13:09:25 -0500

    Create Numeros.cs

---
+++ Numeros.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Heap-allocated int vector
+            int[] numeros = new int[5]
+                {45, 12, 6, -8, 56};
+            Console.WriteLine($"elemento en [0]: {numeros[0]}");
+             
+        }
+    }
+}
